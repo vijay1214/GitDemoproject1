4a640e00edd73ac230671cd020fe6f64fef89ece
vijay <vijay@LAPTOP-R8M025S2>
2021-11-23 19:06:41 +0530

    Created the Project and added to the master branch

---
+++ GitDemoproject1/src/com/demo/Test1.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+	 String s = "Yes";
+		
+	 if(s=="Yes")
+		 System.out.println("yes its ok");
+	 else
+		 System.out.println("not o");
+		
+	}
+
+}
