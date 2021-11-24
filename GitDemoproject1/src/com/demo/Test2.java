86ef31e58ae42a5560dabc0ab5ddfaf80b6358a0
Dnyanesh Korde <Dnyanesh Korde@DESKTOP-7PM1CB8>
2021-11-24 14:30:55 +0530

    Added Test2 and conditions in test2

---
+++ GitDemoproject1/src/com/demo/Test2.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		
+	 String s = "Yes";
+		
+	 if(s=="Yes"||s=="y"||s=="yes"||s=="Y")
+		 System.out.println("yes its ok");
+	 else
+		 System.out.println("not o");
+		
+	}
+
+}
